b70c8fb6e382f67a0c20c1748f64f490ba12a203
Laura Kouadio <penseeexprimee@gmail.com>
2025-01-04 17:57:21 +0100

    Add style to the list tile

--- lib/views/notes/list_notes_view.dart
+++ lib/views/notes/list_notes_view.dart
@@ -18,27 +18,42 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return ListView.separated(
       itemCount: notes.length, // the amount of notes to display
       itemBuilder: (context, index){
         final note = notes.elementAt(index);
-        return  ListTile(
-          onTap: (){
-            onTap(note); //when we tape on a note, call the function onTap
-          },
-          title: Text(
-            note.text,
-            maxLines: 1,
-            softWrap: true,
-            overflow: TextOverflow.ellipsis,
-          ),
-          trailing: IconButton(
-            onPressed: () {
-              onDelete(note);
-            },
-            icon: const Icon(Icons.delete)
-          ),
+        final screenWidth = MediaQuery.of(context).size.width;
+        return  Column(
+          children: [
+            const SizedBox(height: 40,),
+            SizedBox(
+              width: screenWidth * 0.9,
+              child: ListTile(
+                onTap: (){
+                  onTap(note); //when we tape on a note, call the function onTap
+                },
+                title: Text(
+                  note.text,
+                  maxLines: 1,
+                  softWrap: true,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                trailing: IconButton(
+                  onPressed: () {
+                    onDelete(note);
+                  },
+                  icon: const Icon(Icons.delete)
+                ),
+                tileColor: Colors.orange[100],
+                shape: RoundedRectangleBorder( //add style to the tile
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+          ],
         );
-      });
+      },
+      separatorBuilder: (context, index)=> const SizedBox(height: 5,)
+    );
   }
 }
