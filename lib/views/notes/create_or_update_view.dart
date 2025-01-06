f29e1eeade2b86149076a307dfafd5aeb65b0e4f
Laura Kouadio <penseeexprimee@gmail.com>
2025-01-06 14:52:11 +0100

    Finally the date and time are working

--- lib/views/notes/create_or_update_view.dart
+++ lib/views/notes/create_or_update_view.dart
@@ -3,6 +3,8 @@ import 'package:rappellemoi/generics/get_arguments.dart';
 import 'package:rappellemoi/services/auth/auth_service.dart';
 import 'package:rappellemoi/services/cloud/cloud_firebase_storage.dart';
 import 'package:rappellemoi/services/cloud/cloud_note.dart';
+import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart';
+import 'package:intl/intl.dart';
 import 'dart:developer' as devtools show log;
 
 class CreateOrUpdateNotesView extends StatefulWidget {
@@ -39,18 +41,27 @@ class _CreateOrUpdateNotesViewState extends State<CreateOrUpdateNotesView> {
       _note = existingNote;
       _textController.text = existingNote.text;
       
-      devtools.log('Existing note controller: ${existingNote.notificationDate}');
-      devtools.log('Date time controller: ${_dateTimeController.text}');
 
       if(flag == 1 ){ //we just called selected date
         devtools.log('flag equal to one');
+        devtools.log(_dateTimeController.text);
         await _notesService.updateNoteTime(noteId: existingNote.noteId, notificationDate: DateTime.parse(_dateTimeController.text));
         flag =0;
         return existingNote;
     
       } else{
         _date = existingNote.notificationDate;
-        _dateTimeController.text = existingNote.notificationDate.toString().split(" ")[0];
+        if(_date != null){
+          devtools.log("HEEEEEEEEEEEEERE");
+          devtools.log(_date.toString());
+          var finalDate = DateFormat('d MMMM yyyy HH:mm').format(_date!);
+          devtools.log(finalDate);
+          _dateTimeController.text = finalDate;
+          return existingNote;
+        }
+        
+        var splitTime = '${_date.toString().split(" ")[0]} ${_date.toString().split(" ")[1].substring(0,5)}';
+        _dateTimeController.text = splitTime;
         return existingNote;
     }
       }
@@ -84,10 +95,17 @@ class _CreateOrUpdateNotesViewState extends State<CreateOrUpdateNotesView> {
     if(currentNote !=null && _textController.text.isNotEmpty){
       devtools.log('Save my note please :D');
       devtools.log("DateTime text: ${_dateTimeController.text}");
+      
+
+      //Parse the string into a datetime object
+      DateFormat inputFormat = DateFormat('d MMMM yyyy HH:mm');
+      DateTime parsedDate = inputFormat.parse(_dateTimeController.text);
+      devtools.log("Datetime parse: ${DateTime.parse(parsedDate.toString())}");
+
       await _notesService.updateNote(
         noteId: currentNote.noteId, 
         text: _textController.text,
-        notificationDate: DateTime.parse(_dateTimeController.text)
+        notificationDate: parsedDate
       );
     }
   }
@@ -114,10 +132,15 @@ class _CreateOrUpdateNotesViewState extends State<CreateOrUpdateNotesView> {
     if(currentNote == null){ //there is no note
       return;
     }
+
+    //Parse the string into a datetime object
+      DateFormat inputFormat = DateFormat('d MMMM yyyy HH:mm');
+      DateTime parsedDate = inputFormat.parse(_dateTimeController.text);
+      
     await _notesService.updateNote(
       noteId: currentNote.noteId,
       text: _textController.text,
-      notificationDate: DateTime.parse(_dateTimeController.text)
+      notificationDate: parsedDate
     );
   }
 
@@ -186,6 +209,7 @@ class _CreateOrUpdateNotesViewState extends State<CreateOrUpdateNotesView> {
                       controller: _dateTimeController,
                       onTap:() {
                         _selectedDate();
+
                       },
                       decoration: const InputDecoration(
                         hintText: 'DATE',
@@ -208,23 +232,21 @@ class _CreateOrUpdateNotesViewState extends State<CreateOrUpdateNotesView> {
   }
   Future <void> _selectedDate() async {
     devtools.log('selected date called');
-  DateTime? _picked = await showDatePicker(
-    context: context,
-    initialDate: DateTime.now(),
-    firstDate: DateTime.now(),
-    lastDate: DateTime(2100)
+    DateTime? _picked = await DatePicker.showDateTimePicker(
+    context,
+    showTitleActions: true,
+    minTime: DateTime.now(),
+    maxTime: DateTime(3000,1,1),
+    onConfirm: (time) {
+      devtools.log("_selecteDate function date: ${time.toString().split(" ")[0]}");
+      setState((){
+        var finalDate = DateFormat('d MMMM yyyy HH:mm').format(time);
+        _dateTimeController.text = finalDate;
+        flag = 1;
+      });
+    },
   );
 
-  if (_picked != null){
-    devtools.log("_selecteDate function date: ${_picked.toString().split(" ")[0]}");
-    setState((){
-      _dateTimeController.text = _picked.toString().split(" ")[0];
-      //flag = 1;
-    });
-      flag = 1;
-    devtools.log("Datetime controller ${_dateTimeController.text}");
-
-  }
   return;
 }
 }
